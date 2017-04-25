8b2c0394f2bb439495ba11274d762a60ff829248
ghassen <ghassen.benhadjsalah@supcom.tn>
2017-04-25 16:13:00 +0200

    advance on broadcasted eid

---
+++ Eddystone/EidBroadcasted.js
@@ -0,0 +1,37 @@
+/**
+ * Given the identity key, the beacon rotation exponent, and the times of
+ registration for the beacon and the service, this computes the eid that the
+ beacon is currently broadcasting.
+ */
+
+
+const eid_manager =require('../Eddystone/EidComputation') ;
+
+exports.GetEidBroadcasted = function (AESkey, scaler, beacon_initial_time_seconds,service_initial_time_seconds, callback) {
+
+
+    var d = new Date();
+    var current_seconds = Math.round(d.getTime() / 1000);
+    var beacon_time_seconds = beacon_initial_time_seconds + (current_seconds - service_initial_time_seconds) ;
+    var quantum = Math.floor(beacon_time_seconds / Math.pow(2,scaler)) ;
+    console.log("Beacon time in seconds:",beacon_time_seconds) ;
+    console.log("Beacon quantum:",quantum) ;
+    console.log("Start of quantum:",quantum * Math.pow(2,scaler)) ;
+    console.log("End of quantum:",(quantum+1) * Math.pow(2,scaler)) ;
+
+    eid_manager.GetEid(AESkey,scaler,beacon_time_seconds,function (broadcasted_eid) {
+
+        console.log("Broadcasted ephemeral id: ",broadcasted_eid) ;
+        callback(broadcasted_eid) ;
+
+
+
+
+    }) ;
+
+
+
+
+
+
+} ;
